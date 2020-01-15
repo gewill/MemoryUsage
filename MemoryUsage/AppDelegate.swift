65140d9ed3f90366220e30847e95919e69fc8570
Ge Will <531sunlight@gmail.com>
2020-01-15 14:53:33 +0800

    Add increase memory test

--- MemoryUsage/AppDelegate.swift
+++ MemoryUsage/AppDelegate.swift
@@ -10,9 +10,6 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -32,6 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
+        print(#function, DoraemonMemoryUtil.useMemoryForApp())
+    }
 }
-
--- MemoryUsage/ContentView.swift
+++ MemoryUsage/ContentView.swift
@@ -9,13 +9,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var useMemoryForApp: Int = DoraemonMemoryUtil.useMemoryForApp()
+    @State var totalMemoryForDevice: Int = DoraemonMemoryUtil.totalMemoryForDevice()
+
+    @State var data: [String: Data] = [:]
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            VStack(spacing: 12) {
+                Text("已使用内存:\(useMemoryForApp)MB, 亦:\(useMemoryForApp.toKilo)GB")
+                Text("设备总内存:\(totalMemoryForDevice)MB, 亦:\(totalMemoryForDevice.toKilo)GB")
+                Button(action: {
+                    self.updateUI()
+                }) {
+                    Text("获取内存使用情况")
+                }
+                Button(action: {
+                    self.increaseMemory(1024)
+                    self.updateUI()
+                }) {
+                    Text("增加1GB内存使用")
+                }
+                Button(action: {
+                    self.increaseMemory(100)
+                    self.updateUI()
+                }) {
+                    Text("增加100MB内存使用")
+                }
+                Button(action: {
+                    self.increaseMemory(10)
+                    self.updateUI()
+                }) {
+                    Text("增加10MB内存使用")
+                }
+                Button(action: {
+                    self.increaseMemory(1)
+                    self.updateUI()
+                }) {
+                    Text("增加1MB内存使用")
+                }
+            }
+        }
+    }
+
+    private func increaseMemory(_ count: Int) {
+        data[UUID().uuidString] = Data(repeating: 1, count: count * 1024 * 1024)
+    }
+
+    private func updateUI() {
+        useMemoryForApp = DoraemonMemoryUtil.useMemoryForApp()
+        totalMemoryForDevice = DoraemonMemoryUtil.totalMemoryForDevice()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension Int {
+    var toKilo: String {
+        return String(format: "%.2f", Double(self) / 1024)
     }
 }
